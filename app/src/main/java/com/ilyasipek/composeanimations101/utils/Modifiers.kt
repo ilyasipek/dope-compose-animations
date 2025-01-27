6b0584c51dd8a73eb0dcb08739ea198979236070
ilyasipek <ilyasipek1997@gmail.com>
2025-01-27 17:10:54 +0300

    Add clickable modifier that disables ripple

--- app/src/main/java/com/ilyasipek/composeanimations101/utils/Modifiers.kt
+++ app/src/main/java/com/ilyasipek/composeanimations101/utils/Modifiers.kt
@@ -1,8 +1,15 @@
 package com.ilyasipek.composeanimations101.utils
 
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.IndicationNodeFactory
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.Dp
 
 fun Modifier.horizontalPadding(
@@ -12,3 +19,37 @@ fun Modifier.horizontalPadding(
 fun Modifier.verticalPadding(
     padding: Dp
 ): Modifier = this.padding(vertical = padding)
+
+fun Modifier.clickable(
+    enabled: Boolean = true,
+    rippleEnabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit
+): Modifier = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "clickable"
+        properties["enabled"] = enabled
+        properties["onClickLabel"] = onClickLabel
+        properties["role"] = role
+        properties["onClick"] = onClick
+    }
+) {
+    val localIndication = if (rippleEnabled) LocalIndication.current else null
+    val interactionSource = if (localIndication is IndicationNodeFactory) {
+        // We can fast path here as it will be created inside clickable lazily
+        null
+    } else {
+        // We need an interaction source to pass between the indication modifier and clickable, so
+        // by creating here we avoid another composed down the line
+        remember { MutableInteractionSource() }
+    }
+    Modifier.clickable(
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        onClick = onClick,
+        role = role,
+        indication = localIndication,
+        interactionSource = interactionSource
+    )
+}
